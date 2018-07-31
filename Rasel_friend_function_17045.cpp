9fa87509536d2caccdd34962d5d8f9163a6860e2
Saiful Islam Rasel <s.i.r.computerengineer.24@gmail.com>
2018-07-31 12:13:32 +0600

    Global function as friend program

---
+++ Rasel_friend_function_17045.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Second;
+class First{
+    int num1;
+public:
+    First(){
+        num1=0;
+    }
+    First(int n){
+        num1=n;
+    }
+    friend void add(First ob1,Second ob2);
+};
+
+class Second{
+    int num2;
+public:
+    Second(){
+        num2=0;
+    }
+    Second(int n){
+        num2=n;
+    }
+    friend void add(First ob1,Second ob2);
+};
+
+void add(First ob1,Second ob2){
+    cout<<ob1.num1+ob2.num2<<endl;
+}
+
+int main(){
+    First ob1(5);
+    Second ob2(7);
+    add(ob1,ob2);
+    return 0;
+}
