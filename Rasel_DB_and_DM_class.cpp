260838eaba0f2a6e518ae6e794cd6423c8b82607
Saiful Islam Rasel <s.i.r.computerengineer24@gmail.com>
2018-08-14 19:15:33 +0600

    program 9 of part a: complete the program

--- Rasel_DB_and_DM_class.cpp
+++ Rasel_DB_and_DM_class.cpp
@@ -19,7 +19,7 @@ public:
         centimeter = cm;
     }
     void display(){
-        cout<< "Distance is "<<meter <<" meter"<<centimeter<< " centimeter."<< endl;
+        cout<< "Distance is "<<meter <<" meter "<<centimeter<< " centimeter."<< endl;
     }
     friend DM add(DM ob1,DB ob2);
     friend DB add(DB ob1,DM ob2);
@@ -37,14 +37,43 @@ public:
         inch = in;
     }
     void display(){
-        cout<< "Distance is "<<foot <<" foot"<<inch<< " inch."<< endl;
+        cout<< "Distance is "<<foot <<" foot "<<inch<< " inch."<< endl;
     }
     friend DM add(DM ob1,DB ob2);
     friend DB add(DB ob1,DM ob2);
 };
 
+DM add(DM ob1,DB ob2){
+    DM temp;
+    temp.meter = ob1.meter;
+    temp.centimeter = ob1.centimeter+((ob2.foot*30.48)+(ob2.inch*2.54));
+    if(temp.centimeter>=100) {
+        temp.meter+=(temp.centimeter/100.0);
+        temp.centimeter = temp.centimeter%100;
+    }
+    return temp;
+}
+DB add(DB ob1,DM ob2){
+    DB temp;
+    temp.foot = ob1.foot;
+    temp.inch = ob1.inch+((ob2.centimeter*0.3937)+(ob2.meter*39.37));
+    if(temp.inch>=12){
+        temp.foot+=(temp.inch/12.0);
+        temp.inch=temp.inch%12;
+    }
+    return temp;
+}
+
 int main(){
-    DM ob1(10,55);
-    DB ob2(23,10);
+    DM ob1,ob2;
+    DB ob3,ob4;
+    ob1.input(10,55);
+    ob3.input(23,10);
+
+    ob2 = add(ob1,ob3);
+    ob2.display();
+
+    ob4 = add(ob3,ob1);
+    ob4.display();
     return 0;
 }
