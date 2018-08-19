36e37e257362a7291d67df998223a309e3d13fae
Saiful Islam Rasel <s.i.r.computerengineer.24@gmail.com>
2018-08-19 21:14:45 +0600

    program 6 part B: file copy from one another

---
+++ part_B_program_6.cpp
@@ -0,0 +1,31 @@
+/*//////////////////
+//  Part - B      //
+//  Program - 6   //
+//////////////////*/
+
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+    ifstream inf("in.txt");
+    ofstream outf("out.txt");
+
+    char name[100],roll[100],section[100];
+    int marks[6];
+
+    inf.getline(name,100);
+    inf.getline(roll,100);
+    inf.getline(section,100);
+    for(int i=0;i<5;i++){
+        inf>>marks[i];
+    }
+
+    outf<<name<<"\n";
+    outf<<roll<<"\n";
+    outf<<section<<"\n";
+    for(int i=0;i<5;i++){
+        outf<<marks[i]<< " ";
+    }
+    return 0;
+}
