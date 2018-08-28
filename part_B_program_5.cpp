8ad498e7289031cbadf34fbec17d43ed6b136bea
Saiful Islam Rasel <s.i.r.computerengineer.24@gmail.com>
2018-08-28 11:53:30 +0600

    part B: program 5

---
+++ part_B_program_5.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+
+class polar {
+    double radius;
+    double angle;
+    double getx() {
+      return radius*cos(angle);
+    }
+    double gety() {
+        return radius*sin(angle);
+    }
+public:
+    polar() {
+       radius=0.0;angle=0.0;
+    }
+
+    polar(double r,double a) {
+      radius = r;
+      angle = a;
+    }
+
+    void display() {
+      printf("Radius = %.2f, Angle = %.2f\n",radius,angle);
+    }
+
+    polar operator + (polar o2) {
+        polar temp;
+        double x=getx()+o2.getx();
+        double y=gety()+o2.gety();
+        temp.radius = sqrt(x*x + y*y);
+        temp.angle = atan(y/x);
+        return temp;
+    }
+};
+
+int main() {
+     polar o1(10.0,2.0),o2(10.0,5.0),o3;
+
+     o3=o1+o2;
+
+     printf("object 1: ");
+     o1.display();
+     printf("object 2: ");
+     o2.display();
+     printf("object 3: ");
+     o3.display();
+
+     return 0;
+}
