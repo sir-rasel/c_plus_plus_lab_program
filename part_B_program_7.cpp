1a945750f5b67b8db892876ae7b2cf2a616fdab9
Saiful Islam Rasel <s.i.r.computerengineer.24@gmail.com>
2018-10-24 23:40:41 +0600

    part B program 7

---
+++ part_B_program_7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+    ifstream fin("withSpace.txt");
+    ofstream fout("withoutSpace.txt");
+    string s;
+    while(fin>>s){
+        fout<<s;
+    }
+    return 0;
+}
